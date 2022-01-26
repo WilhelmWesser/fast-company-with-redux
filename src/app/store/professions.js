cd429187bdd15effff93341da2443716bcfe06dc
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-01-26 10:21:43 +0200

    Task 18(part 1) is done

---
+++ src/app/store/professions.js
@@ -0,0 +1,75 @@
+import { createSlice } from "@reduxjs/toolkit";
+import professionService from "../services/profession.service";
+
+const professionsSlice = createSlice({
+    name: "professions",
+    initialState: {
+        entities: null,
+        isLoading: true,
+        error: null,
+        lastFetch: null
+    },
+    reducers: {
+        professionsRequested: (state) => {
+            state.isLoading = true;
+        },
+        professionsReceived: (state, action) => {
+            state.entities = action.payload;
+            state.lastFetch = Date.now();
+            state.isLoading = false;
+        },
+        professionsRequestFailed: (state, action) => {
+            state.error = action.payload;
+            state.isLoading = false;
+        }
+    }
+});
+
+const { reducer: professionsReducer, actions } = professionsSlice;
+const { professionsRequested, professionsReceived, professionsRequestFailed } =
+    actions;
+
+function isOutdated(date) {
+    if (Date.now() - date > 10 * 60 * 1000) {
+        return true;
+    }
+    return false;
+}
+
+export const loadProfessionsList = () => async (dispatch, getState) => {
+    const { lastFetch } = getState().professions;
+    if (isOutdated(lastFetch)) {
+        // console.log(lastFetch);
+        dispatch(professionsRequested());
+        try {
+            const { content } = await professionService.get();
+            console.log(content);
+            dispatch(professionsReceived(content));
+        } catch (error) {
+            dispatch(professionsRequestFailed(error.message));
+        }
+    }
+};
+
+export const getProfessions = () => (state) => {
+    console.log(state.professions);
+    console.log(state.qualities);
+    return state.professions.entities;
+};
+export const getProfessionsLoadingStatus = () => (state) =>
+    state.professions.isLoading;
+export const getProfessionById = (professionId) => (state) => {
+    if (state.professions.entities) {
+        let professionToGet;
+        for (const profession of state.professions.entities) {
+            if (profession._id === professionId) {
+                professionToGet = profession;
+                break;
+            }
+        }
+        return professionToGet;
+    }
+    return null;
+};
+
+export default professionsReducer;
